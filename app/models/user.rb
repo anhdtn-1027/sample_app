7fdb4f2237ef8a7cfafc3fcbd6777bfcff3f91f5
anhdtn-1027 <dang.thi.ngoc.anh@sun-asterisk.com>
2020-07-17 13:47:17 +0700

    Chapter 9. Advanced Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   USER_PARAMS = %i(name email password password_confirmation).freeze
 
+  attr_accessor :remember_token
+
   validates :name, presence: true,
     length: {maximum: Settings.user.name.length.maximum}
   validates :email, presence: true,
@@ -22,6 +24,25 @@ class User < ApplicationRecord
              end
       BCrypt::Password.create string, cost: cost
     end
+
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
   end
 
   private
