37281cfd1b77e314dfd129330a387c8fa1516d3b
anhdtn-1027 <dang.thi.ngoc.anh@sun-asterisk.com>
2020-07-17 13:47:17 +0700

    Chapter 11. Activate Account

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
-      log_in user
-      check_remember user
-      flash[:success] = t "users.success_user"
-      redirect_back_or user
+      activate user
     else
       flash.now[:danger] = t "users.invalid_user"
       render :new
@@ -17,5 +14,19 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def activate user
+    if user.activated?
+      log_in user
+      check_remember user
+      flash[:success] = t "users.success_user"
+      redirect_back_or user
+    else
+      flash[:warning] = t "users.mailer.activate_warning"
+      redirect_to root_url
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   USER_PARAMS = %i(name email password password_confirmation).freeze
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   validates :name, presence: true,
     length: {maximum: Settings.user.name.length.maximum}
@@ -13,6 +13,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  before_create :create_activation_digest
   before_save :downcase_email
 
   class << self
@@ -35,19 +36,33 @@ class User < ApplicationRecord
     update :remember_digest, User.digest(remember_token)
   end
 
-  def authenticated? remember_token
-    return false unless remember_digest
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false unless digest
 
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+    BCrypt::Password.new(digest).is_password? token
   end
 
   def forget
     update remember_digest: nil
   end
 
+  def activate
+    update activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
   def downcase_email
     email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest activation_token
   end
 end
